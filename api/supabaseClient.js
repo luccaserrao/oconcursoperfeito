34a78f7c0f3d7837ce20cbfe9b4259506c9e345b
Lucca Serrao <luccahenique@gmail.com>
2025-12-05 15:28:44 -0300

    fix supabase

--- api/supabaseClient.js
+++ api/supabaseClient.js
@@ -1,10 +1,12 @@
-import { createClient } from '@supabase/supabase-js';
+const { createClient } = require("@supabase/supabase-js");
 
 const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
 const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
 
 if (!supabaseUrl || !supabaseAnonKey) {
-  throw new Error("Supabase env vars not set");
+  throw new Error("Supabase URL ou Anon Key não configurados.");
 }
 
-export const supabase = createClient(supabaseUrl, supabaseAnonKey);
+const supabase = createClient(supabaseUrl, supabaseAnonKey);
+
+module.exports = { supabase };
